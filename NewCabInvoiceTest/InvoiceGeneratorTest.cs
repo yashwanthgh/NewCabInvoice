81c6e3e7b9bd7ea483e3156a94694603059cacc1
yashwanthgh <yugnasura@gmail.com>
2024-04-12 11:26:19 +0530

    Initilising the project

---
+++ NewCabInvoiceTest/InvoiceGeneratorTest.cs
@@ -0,0 +1,17 @@
+namespace NewCabInvoiceTest
+{
+
+    public class InvoiceGeneratorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
